599f13aca052b7e57ef6bd8432247791f0af2f54
Rohit <rohhit.rz@gmail.com>
2024-09-16 03:32:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import Modal from './components/Xmodal';
 
 function App() {
--- src/components/Xmodal.jsx
+++ src/components/Xmodal.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './modal.css'
 
 const validateEmail=(email)=>{
     return email.includes('@');
@@ -24,11 +25,132 @@ export default function Modal (){
         phone:"",
         dob:"" 
     })
+    const [error,setError]=useState('');
+
+    const toggleModal=()=>{
+        setIsOpen(!isOpen)
+        setError('');
+    }
+
+    const handleChange=(e)=>{
+        const {id,value}=e.target;
+        setFormData((prevData)=>(
+            {...prevData,
+                [id]:value,
+
+            }
+        ))
+    
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        const {username,email,phone,dob}= formData;
+
+        if(!username || !email|| !phone || !dob){
+            setError('All fields are required.');
+            return;
+        }
+
+        if(!validateDob(dob)){
+            alert('Invalid date of birth. Please enter a valid past date.');
+            return;
+        }
+        if(!validateEmail(email)){
+            alert('Invalid email. Please check your email address.');
+            return;
+        }
+        if(!validatePhoneNumber(phone)){
+            alert('Invalid phone number. Please enter a 10-digit phone number.');
+            return;
+        }
+
+        alert("Form Submitted Successfully!");
+        toggleModal();
+        setFormData({username: '', email:'', phone:'',dob:''});
+  }
+    const handleOutsideClick=(e)=>{
+        if(e.target.className==='modal'){
+            toggleModal();
+        }
+
+    }
 
 
 
     return(
-        <button>Open Form</button>
+        <div>
+        <button onClick={toggleModal}>Open Form</button>
+
+        {isOpen&&(
+            <div className="modal" onClick={handleOutsideClick}>
+            <div className="modal-content">
+            <form onSubmit={handleSubmit}>
+
+            <div>
+            <label htmlFor="username">Username:</label>
+            <input 
+            type="text" 
+            id="username"
+            value={formData.username}
+            onChange={handleChange}
+            
+            />
+            </div>
+            <div>
+
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" 
+            id="email"
+            value={formData.email}
+            onChange={handleChange} />
+            </div>
+
+            <div>
+            <label htmlFor="phone">Phone:</label>
+            <input type="number"
+            id="phone"
+            value={formData.phone}
+            onChange={handleChange} />
+            
+            
+            </div>
+
+            <div>
+            <label htmlFor="dob">Date of Birth:</label>
+            <input type="date"
+            id="dob"
+            value={formData.dob}
+            onChange={handleChange} />
+            
+            </div>
+
+            {error && <p style={{color: "red"}}>{error}</p> }
+            <button type="submit" className="submit-button">Submit</button>
+
+
+            
+
+            
+            
+            
+            
+            
+            </form>
+            
+            
+            </div>
+            
+            
+            </div>
+
+
+
+
+        )}
+        </div>
 
 
 
